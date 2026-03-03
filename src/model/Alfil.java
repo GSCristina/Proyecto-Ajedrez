dbd500a4b2a42aa8e6262f89c1745cb319968571
Cristina Civico <cristina.civico@sepalo.es>
2026-03-03 12:35:06 +0100

    Creadas piezas, falta meter puntos (se requiere el metodo obtenerPuntos) para realizarlo

--- src/model/Alfil.java
+++ src/model/Alfil.java
@@ -1,6 +1,10 @@
 package model;
 
-public class Alfil extends Pieza0 {
+public class Alfil extends Pieza {
+    public Alfil(int fila, int columna, Color color, int valor, char caracter) {
+        super(fila, columna, color, valor, caracter);
+    }
+
     @Override
     public String toString() {
         return this.getColor() == Color.BLANCA ? "♗" : "♝";
--- src/model/Rey.java
+++ src/model/Rey.java
@@ -1,6 +1,10 @@
 package model;
 
-public class Rey extends Pieza0 {
+public class Rey extends Pieza {
+    public Rey(int fila, int columna, Color color, int valor, char caracter) {
+        super(fila, columna, color, valor, caracter);
+    }
+
     @Override
     public String toString() {
         return this.getColor() == Color.BLANCA ? "♔" : "♚";
