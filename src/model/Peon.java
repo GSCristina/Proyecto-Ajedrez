272c75eac4e4b8b7022dcfaad984207874d8d65f
JoseAntonioCD <joseantoniocastillero@gmail.com>
2026-03-05 12:59:34 +0100

    Commit:      - Movimiento Peón

--- src/model/Peon.java
+++ src/model/Peon.java
@@ -7,6 +7,47 @@ public class Peon extends Pieza {
 
     @Override
     public boolean puedeMover(int nuevaFila, int nuevaColumna, Tablero tablero) {
+
+        if(fila<0 || fila >7 || columna < 0 || columna >7){
+            return false;
+        }
+
+        int direccion;  //Dirección en la que se puede mover el peón.
+        int filaInicial;    //Fila en la que empieza el peón.
+
+        /**
+         * Determina la dirección en las que se mueven y la fila inicial de los peones basandose en el color.
+         */
+
+        if(getColor() == Color.BLANCA){
+            direccion = -1;
+            filaInicial = 6;
+        }else{
+            direccion = 1;
+            filaInicial = 1;
+        }
+        int diferenciaFila = nuevaFila - getFila();
+        int diferenciaColumna = nuevaColumna - getColumna();
+        Pieza destino = tablero.obtenerPieza(nuevaFila,nuevaColumna);
+
+        if(diferenciaColumna == 0){
+            /**
+             * Avance de una casilla
+             */
+            if(diferenciaFila == direccion && destino == null){
+                return true;
+            }
+
+            /**
+             * Avance de dos casillas(Solo en posición original)
+             */
+            if (getFila() == filaInicial &&
+                    diferenciaFila == 2 * direccion &&
+                    destino == null &&
+                    tablero.obtenerPieza(getFila() + direccion, getColumna()) == null) {
+                return true;
+            }
+        }
         return false;
     }
 
