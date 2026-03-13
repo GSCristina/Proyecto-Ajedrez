c38e0f07d506010db539b08af2426e3a90a616d1
Cristina Civico <cristina.civico@sepalo.es>
2026-03-13 18:21:54 +0100

    feat: Cambiado el método puedeMover y copiarPieza

--- src/main/java/model/Alfil.java
+++ src/main/java/model/Alfil.java
@@ -6,9 +6,26 @@ public class Alfil extends Pieza {
         super(fila, columna, color, puntos);
     }
 
+    /**
+     * Comprueba si el Alfil puede moverse a la casilla de destino.
+     * La diferencia de filas debe ser igual a la diferencia de columnas.
+     * @param nuevaFila La fila de destino.
+     * @param nuevaColumna La columna de destino.
+     * @param tablero El tablero actual para comprobar obstáculos.
+     * @return true si el movimiento es diagonal y el camino está libre, false en caso contrario.
+     */
     @Override
     public boolean puedeMover(int nuevaFila, int nuevaColumna, Tablero tablero) {
-        return false;
+        int difFila = Math.abs(this.getFila() - nuevaFila);
+        int difColumna = Math.abs(this.getColumna() - nuevaColumna);
+
+        if (difFila == 0 || difFila != difColumna) {
+            return false;
+        }
+        if (tablero.hayPiezasEntre(this.getFila(), this.getColumna(), nuevaFila, nuevaColumna)) {
+            return false;
+        }
+        return true;
     }
 
     @Override
@@ -18,7 +35,7 @@ public class Alfil extends Pieza {
 
     @Override
     public Pieza copiarPieza() {
-        return new Torre(this.getFila(), this.getColumna(), this.getColor(), this.puntos);
+        return new Alfil(this.getFila(), this.getColumna(), this.getColor(), this.puntos);
     }
 
     @Override
