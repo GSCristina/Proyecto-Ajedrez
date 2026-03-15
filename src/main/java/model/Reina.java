06794aadbc841697ae42a5a8f0b7bdbac63613a4
JL-MD <morenodobao@gmail.com>
2026-03-15 19:45:06 +0100

    Feat:He creado los metodos puedeMover y copiarPieza para la reina

--- src/main/java/model/Reina.java
+++ src/main/java/model/Reina.java
@@ -5,9 +5,29 @@ public class Reina extends Pieza {
         super(fila, columna, color, puntos);
     }
 
+
+    /**
+     * Comprueba si la Reina puede moverse a una casilla.
+     * La Reina se puede moverse en línea recta (horizontal, vertical o diagonal),
+     * siempre y cuando no haya otras piezas bloqueando su trayectoria.
+     * @param nuevaFila La fila del tablero a la que se quiere mover la Torre.
+     * @param nuevaColumna La columna del tablero a la que se quiere mover la Torre.
+     * @param tablero El tablero actual, necesario para comprobar si hay obstáculos en el camino.
+     * @return true si el movimiento es legal para la Reina; false si es inválido o está bloqueado.
+     */
     @Override
     public boolean puedeMover(int nuevaFila, int nuevaColumna, Tablero tablero) {
-        return false;
+        if (this.getFila() == nuevaFila && this.getColumna() == nuevaColumna) {
+            return false;
+        }
+        if (this.getFila() == nuevaFila || this.getColumna() == nuevaColumna) {
+            return !tablero.hayPiezasEntre(this.getFila(), this.getColumna(), nuevaFila, nuevaColumna);
+        }
+        // Logica de movimiento diagonal se deberia de poder reusar para el alfil(borrar esta linea despues de revisar)
+        if (Math.abs(nuevaFila - this.getFila()) == Math.abs(nuevaColumna - this.getColumna())) {
+            return !tablero.hayPiezasEntre(this.getFila(), this.getColumna(), nuevaFila, nuevaColumna);
+        }
+            return false;
     }
 
     @Override
@@ -17,10 +37,13 @@ public class Reina extends Pieza {
 
     @Override
     public Pieza copiarPieza() {
-        return null;
+        return new Reina(this.getFila(), this.getColumna(), this.getColor(), this.puntos);
     }
 
     @Override
     protected char simbolo() {return this.getColor() == Color.BLANCA ? '♕' : '♛';}
-
 }
+
+
+
+
