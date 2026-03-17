39ca0f50bf78fb22cd767c81843293a92be68c97
Cristina Civico <cristina.civico@sepalo.es>
2026-03-17 19:55:44 +0100

    fix: Comentado enum y piezaSaltadora

--- src/main/java/model/Color.java
+++ src/main/java/model/Color.java
@@ -1,5 +1,9 @@
 package model;
 
+/**
+ * Representa los equipos disponibles para el juego (Blancas o Negras) y para controlar con él el turno
+ * de la partida
+ */
 public enum Color {
     BLANCA,
     NEGRA
--- src/main/java/model/PiezaSaltadora.java
+++ src/main/java/model/PiezaSaltadora.java
@@ -1,4 +1,9 @@
 package model;
 
+/**
+ * Interfaz pensada para piezas con la capacidad de saltar otras, esta pensada para ser implentada en la
+ * pieza caballo, las clases que implementen esta interfaz no requieren comprobar si hay piezas en el
+ * recorrido del movimiento de dicha pieza.
+ */
 public interface PiezaSaltadora {
 }
